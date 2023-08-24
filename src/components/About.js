7419ed46844a8cb2beb743467762774e121f5451
Jason Guba <jiguba@uchicago.edu>
2023-08-23 21:39:34 -0500

    Updated about blurb

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,9 @@ export default function About(props) {
                 <img className="image" id="profile_picture" src={props.image} alt =""/>
                 <h2>Hello!</h2>
                 <p>
-                I am currently a fourth year student studying computer science at
-                the University of Chicago! Prior to my studies, I served in the
-                United States Marine Corps as an electronics technician. I am
-                currently interested in pursuing a career in full-stack web
-                development.
+                I am recent graduate of the University of Chicago where I studied computer science! 
+                Prior to my studies, I served in the United States Marine Corps as an electronics 
+                technician. I am currently interested in pursuing a career in full-stack development.
                 </p>
             </div>
         </div>
