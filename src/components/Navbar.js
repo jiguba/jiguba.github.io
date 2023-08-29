3af9e07d13cfaaee61f459eb6f8f0a6c4e62516e
Jason Guba <jiguba@uchicago.edu>
2023-08-28 23:42:26 -0500

    Fixed hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+
 export default function Navbar() {
+
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" onClick={() => setShowMenu(prev => !prev)} data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+        <div className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`} id="navbarTogglerDemo01">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href="#about">About</a>
@@ -15,6 +21,7 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
+
       </div>
   </nav>);
 }
